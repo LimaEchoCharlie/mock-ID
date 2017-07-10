822c72542a2e9c21000666c8848b1de00fb522b3
LimaEchoCharlie <james@simprints.com>
2017-07-10 16:45:33 +0100

    Basic register and identify mock

---
+++ app/src/main/java/io/github/limaechocharlie/mockid/IdentifyActivity.kt
@@ -0,0 +1,30 @@
+package io.github.limaechocharlie.mockid
+
+import android.app.Activity
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.simprints.libsimprints.Constants
+import kotlinx.android.synthetic.main.activity_identify.*
+import android.content.Intent
+
+
+
+class IdentifyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_identify)
+
+        api_key.text = getString(R.string.api_key, getIntent().getStringExtra(Constants.SIMPRINTS_API_KEY))
+        module_id.text = getString(R.string.module_id, getIntent().getStringExtra(Constants.SIMPRINTS_MODULE_ID))
+        user_id.text = getString(R.string.user_id, getIntent().getStringExtra(Constants.SIMPRINTS_USER_ID))
+        metadata.text = getString(R.string.metadata, getIntent().getStringExtra(Constants.SIMPRINTS_METADATA))
+
+        return_to_caller.setOnClickListener {
+            //val resultData = Intent(Constants.SIMPRINTS_IDENTIFY_INTENT)
+            //resultData.putExtra(Constants.SIMPRINTS_REGISTRATION, registrationResult)
+            setResult(Constants.SIMPRINTS_CANCELLED)//, resultData)
+            finish()
+        }
+    }
+}
